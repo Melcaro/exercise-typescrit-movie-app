bddeb3685eb443c10d7ab1a8bd247263922457c9
Melanie <melanie.caro78@gmail.com>
2020-03-10 12:41:41 +0100

    --no-edit

--- client/src/services/fetchDataServices.ts
+++ client/src/services/fetchDataServices.ts
@@ -1,38 +1,39 @@
 import axios from 'axios';
 import * as Types from '../../../commons/Types';
 
+const URL = "/api/v1"
 export async function getMoviesList() {
-  const { data: results } = await axios.get<Types.IResult[]>(`/movies`);
+  const { data: results } = await axios.get<Types.IResult[]>(`${URL}/movies`);
   return results;
 }
 
 export async function getMovieByID(movieID: string) {
-  const { data } = await axios.get<Types.IMovieResult>(`/movies/${movieID}`);
+  const { data } = await axios.get<Types.IMovieResult>(`${URL}/movies/${movieID}`);
   return data;
 }
 
 export async function getMovieCast(movieID: string) {
   const { data } = await axios.get<Types.IActor[]>(
-    `/movies/${movieID}/credits`
+    `${URL}/movies/${movieID}/credits`
   );
   return data;
 }
 
 export async function getActorInfos(actorID: string) {
-  const { data } = await axios.get<Types.IActorInfos>(`/actor/${actorID}`);
+  const { data } = await axios.get<Types.IActorInfos>(`${URL}/actor/${actorID}`);
   return data;
 }
 
 export async function getActorFilmography(actorID: string) {
   const { data } = await axios.get<Types.IActorFilmography[]>(
-    `/actor/${actorID}/movie_credits`
+    `${URL}/actor/${actorID}/movie_credits`
   );
   return data;
 }
 
 export async function searchForContent(userQuery: string) {
   const { data } = await axios.get<Types.ISearchResult[]>(
-    `/search/${userQuery}`
+    `${URL}/search/${userQuery}`
   );
   return data;
 }
